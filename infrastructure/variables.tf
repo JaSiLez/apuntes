639499311b96bda30e9b0946b17bc136fbd3570e
bylijoe <leonardonevado.silva>
2022-03-29 20:42:08 +0200

    initial commit

---
+++ infrastructure/variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+  type        = map
+  description = "Name of the project."
+  default     = {
+    dev  = "acme-storage-dev"
+    prod = "acme-storage-prod"
+  }
+}
+
+
+variable "aws_region" {
+}
+
+variable "env" {
+  description = "env: dev or prod"
+}
