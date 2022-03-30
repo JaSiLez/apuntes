491f1a90f93d66c23944ac136fd51bd87f1403a7
bylijoe <91939541+bylijoe@users.noreply.github.com>
2022-03-30 13:30:09 +0200

    supresion region for resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,7 @@ resource "random_id" "tf_bucket_id" {
 # Create the bucket
 resource "aws_s3_bucket" "tf_code" {
   bucket = "${lookup(var.project_name, var.env)}-${random_id.tf_bucket_id.dec}"
-  region = var.aws_region
+  
 
   force_destroy = true
 
